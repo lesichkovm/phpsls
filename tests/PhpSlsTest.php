78e814b48f52923a5c0a791ecc8e69e95f927a02
Milan Lesichkov <lesichkovm@gmail.com>
2019-12-28 09:53:38 +0000

    Initial test suite

---
+++ tests/PhpSlsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class PhpSlsTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testIt(){
+        $this->assertTrue(true);
+    }
+}
