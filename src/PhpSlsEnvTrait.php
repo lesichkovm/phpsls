d3cf4957d47f01b2501523eeb563611a25da8dde
Milan Lesichkov <lesichkovm@gmail.com>
2020-05-20 19:01:32 +0100

    Update PhpSlsEnvTrait.php

--- src/PhpSlsEnvTrait.php
+++ src/PhpSlsEnvTrait.php
@@ -7,10 +7,13 @@ trait PhpSlsEnvTrait {
     /**
      * Deploys an environment to the serverless action
      * specified in its configuration file
+     * Params:
+     * --dry-run=yes
      */
     public function dot($args, $params = []) {
         Native::$logEcho = true;
         $environment = trim(array_shift($args));
+        $dryRun = (trim($params['dry-run'] ?? 'no') == 'yes');
 
         /* 1. Environment */
         if ($environment == "") {
@@ -30,14 +33,29 @@ trait PhpSlsEnvTrait {
         $dotenv = \Dotenv\Dotenv::createMutable($this->dirCwd, [basename($this->fileDotEnvDynamic)]);
         $dotenv->load();
 
-        var_dump($_ENV);
         foreach ($_ENV as $key => $value) {
             $resolvedValue = $this->_valueResolve($value, $environment);
             $_ENV[$key] = $resolvedValue;
         }
-        var_dump($_ENV);
+
+        if ($dryRun == true) {
+            \Sinevia\Utils::alert($_ENV);
+            return true;
+        }
+
+        if (file_exists($this->fileDotEnv)) {
+            $this->say("File " . $this->fileDotEnv . " already exists. FAILED");
+            return false;
+        }
+
+        $envContent = '';
+        foreach ($_ENV as $key => $value) {
+            $envContent .= $key . '=' . json_encode($value) . "\n";
+        }
+        file_put_contents($this->fileDotEnv, $envContent);
+        return true;
     }
-    
+
     function _isFileVariable($str) {
         if (\Sinevia\StringUtils::startsWith($str, '${file:') AND \Sinevia\StringUtils::endsWith($str, '}')) {
             return true;
@@ -54,25 +72,43 @@ trait PhpSlsEnvTrait {
 
     function _getSsmPath($ssmVariable) {
         $ssmPath = substr(substr($ssmVariable, 6), 0, -1);
-        return $ssmPath;
+
+        $cmd = "aws ssm get-parameter --with-decryption --name $ssmPath --region eu-west-2";
+        //Native::$logEcho = true;
+        $result = Native::exec($cmd);
+        $value = json_decode(implode('', Native::$lastExecOut))->Parameter->Value ?? null;
+
+        if (is_null($value)) {
+            $this->say('Error: Amazon SSM key "' . $ssmPath . '" NOT FOUND. FAILED');
+            exit(1);
+        }
+
+        return $value;
     }
-    
+
     function _getFilePath($str) {
         $filePath = substr(substr($str, 7), 0, -1);
-        
+
         $param = \Sinevia\StringUtils::rightFrom($filePath, ":");
         $file = \Sinevia\StringUtils::leftFrom($filePath, ":");
-        
+
+        if (file_exists($file) == false) {
+            $this->say('ERROR: Environment file "' . $file . '" DOES NOT exist. FAILED');
+            exit(1);
+        }
+
         $fileContents = file_get_contents($file);
-        $json = json_decode($fileContents);
-        
-        \Sinevia\Utils::alert($json);
-        
-        return $filePath;
+        $json = json_decode($fileContents, true);
+
+        $dot = new \Adbar\Dot($json);
+
+        $value = $dot->get($param);
+
+        return $value;
     }
 
     function _valueResolve($value, $environment) {
-        if(\Sinevia\StringUtils::hasSubstring($value, '$ENVIRONMENT')){
+        if (\Sinevia\StringUtils::hasSubstring($value, '$ENVIRONMENT')) {
             $value = str_replace('$ENVIRONMENT', $environment, $value);
         }
         if ($this->_isSsmVariable($value)) {
@@ -84,8 +120,4 @@ trait PhpSlsEnvTrait {
         return $value;
     }
 
-    //$cmd = "aws ssm get-parameter --with-decryption --name /test/db/name --region eu-west-2";
-//PHPServerless\Native::$logEcho = true;
-//$result = PHPServerless\Native::exec($cmd);
-//var_dump(json_decode(implode('', PHPServerless\Native::$lastExecOut))->Parameter->Value);
 }
