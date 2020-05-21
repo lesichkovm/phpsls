62eaa8cc10c185f34b99fbf09e11a6d1def16612
Milan Lesichkov <lesichkovm@gmail.com>
2020-05-21 23:27:18 +0100

    Update PhpSlsEnvTrait.php

--- src/PhpSlsEnvTrait.php
+++ src/PhpSlsEnvTrait.php
@@ -14,6 +14,7 @@ trait PhpSlsEnvTrait {
         Native::$logEcho = true;
         $environment = trim(array_shift($args));
         $dryRun = (trim($params['dry-run'] ?? 'no') == 'yes');
+        $force = (trim($params['force'] ?? 'no') == 'yes');
 
         /* 1. Environment */
         if ($environment == "") {
@@ -30,6 +31,8 @@ trait PhpSlsEnvTrait {
             return false;
         }
 
+        //echo $this->fileDotEnvDynamic;
+
         $dotenv = \Dotenv\Dotenv::createMutable($this->dirCwd, [basename($this->fileDotEnvDynamic)]);
         $dotenv->load();
 
@@ -44,16 +47,56 @@ trait PhpSlsEnvTrait {
         }
 
         if (file_exists($this->fileDotEnv)) {
-            $this->say("File " . $this->fileDotEnv . " already exists. FAILED");
-            return false;
+            if ($force == "yes") {
+                $this->say("File " . $this->fileDotEnv . " already exists but force option caller. OVERWRITING");
+            } else {
+                $this->say("File " . $this->fileDotEnv . " already exists. FAILED");
+                return false;
+            }
         }
 
         $envContent = '';
         foreach ($_ENV as $key => $value) {
             $envContent .= $key . '=' . json_encode($value) . "\n";
         }
-        file_put_contents($this->fileDotEnv, $envContent);
-        return true;
+
+        $length = file_put_contents($this->fileDotEnv, $envContent);
+        if ($length !== false) {
+            $this->say("File " . $this->fileDotEnv . " written successfully. SUCCESS");
+            return true;
+        }
+
+        $this->say("File " . $this->fileDotEnv . " failed to be written check permissions. FAILED");
+        return false;
+    }
+
+    function _createDotFileForEnvironment($environment, $destinationFilePath) {
+        if (file_exists($this->fileDotEnvDynamic) == false) {
+            $this->say("File .env.dynamic DOES NOT exist. FAILED");
+            return false;
+        }
+        $dotenv = \Dotenv\Dotenv::createMutable($this->dirCwd, [basename($this->fileDotEnvDynamic)]);
+        $dotenv->load();
+
+        foreach ($_ENV as $key => $value) {
+            $resolvedValue = $this->_valueResolve($value, $environment);
+            $_ENV[$key] = $resolvedValue;
+        }
+
+        $envContent = '';
+        foreach ($_ENV as $key => $value) {
+            $envContent .= $key . '=' . json_encode($value) . "\n";
+        }
+
+
+        $length = file_put_contents($destinationFilePath, $envContent);
+        if ($length !== false) {
+            $this->say("File " . $destinationFilePath . " written successfully. SUCCESS");
+            return true;
+        }
+
+        $this->say("File " . $destinationFilePath . " failed to be written check permissions. FAILED");
+        return false;
     }
 
     function _isFileVariable($str) {
@@ -89,8 +132,17 @@ trait PhpSlsEnvTrait {
     function _getFilePath($str) {
         $filePath = substr(substr($str, 7), 0, -1);
 
+        if (strpos($filePath, ':') == 1) { // Windows drive letter?
+            $filePath = implode('___!!!___', explode(':', $filePath, 2));
+        }
+
         $param = \Sinevia\StringUtils::rightFrom($filePath, ":");
         $file = \Sinevia\StringUtils::leftFrom($filePath, ":");
+        $file = str_replace('___!!!___', ':', $file);
+
+        if (\Sinevia\StringUtils::startsWith($file, '~')) { // User home?
+            $file = str_replace('~', Native::userHome(), $file);
+        }
 
         if (file_exists($file) == false) {
             $this->say('ERROR: Environment file "' . $file . '" DOES NOT exist. FAILED');
