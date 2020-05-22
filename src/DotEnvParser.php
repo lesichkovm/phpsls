47e30f100a97ad0f0bb3345200fb5c0fa5cc029d
Milan Lesichkov <lesichkovm@gmail.com>
2020-05-22 02:48:28 +0100

    Create DotEnvParser.php

---
+++ src/DotEnvParser.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace PHPServerless;
+
+class DotEnvParser {
+
+    /**
+     * .env file to array conversion
+     *
+     * @param $envPath
+     * @return array
+     */
+    public static function envToArray($envPath) {
+        $variables = [];
+        $content = file_get_contents($envPath);
+        $lines = explode("\n", $content);
+        if (!$lines) {
+            return [];
+        }
+
+        foreach ($lines as $line) {
+            if ($line === "") {
+                $variables[] = "";
+                continue;
+            }
+
+            // Find position of first equals symbol
+            $equalsLocation = strpos($line, '=');
+
+            // Pull everything to the left of the first equals
+            $key = substr($line, 0, $equalsLocation);
+
+            // Pull everything to the right from the equals to end of the line
+            $value = substr($line, ($equalsLocation + 1), strlen($line));
+
+            $variables[$key] = trim(trim($value), '"');
+        }
+
+        return $variables;
+    }
+
+    /**
+     * Array to .env file storage
+     *
+     * @param $array
+     * @param $envPath
+     */
+    public static function arrayToEnv($array, $envPath) {
+        $env = "";
+        $position = 0;
+        foreach ($array as $key => $value) {
+            $position++;
+
+            // If value is blank, or key is numeric meaning not a blank line, then add entry
+            if ($value === "" || is_numeric($key)) {
+                $env .= "\n";
+                continue;
+            }
+
+
+            // If passed in option is a boolean (true or false) this will normally
+            // save as 1 or 0. But we want to keep the value as words.
+            if (is_bool($value)) {
+                $value = ($value === true) ? "true" : "false";
+            }
+
+            // Always convert $key to uppercase
+            $env .= strtoupper($key) . "=" . $value;
+
+            // If isn't last item in array add new line to end
+            if ($position != count($array)) {
+                $env .= "\n";
+            }
+        }
+
+        file_put_contents($envPath, $env);
+    }
+
+}
