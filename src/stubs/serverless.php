44ee0db04bae9eb832315105d22112f918d64e42
Milan Lesichkov <lesichkovm@gmail.com>
2020-05-23 08:53:49 +0100

    Update serverless.php

--- src/stubs/serverless.php
+++ src/stubs/serverless.php
@@ -3,17 +3,21 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/main.php';
 
-
-function run($args = [])
-{
+function run($args = []) {
     date_default_timezone_set('Europe/London');
 
     \Sinevia\Serverless::openwhisk($args);
-    
-    return responseHtml(main());
+
+    return runResponseFormat(main());
 }
 
-function responseHtml($html)
-{
-    return ['body' => $html];
+function runResponseFormat($response) {
+    if (is_object($response)) {
+        $response = json_decode(json_encode($response), true);
+    }
+    if (is_array($response) AND isset($response['body'])) {
+        return $response;
+    } else {
+        return ['body' => $response];
+    }
 }
